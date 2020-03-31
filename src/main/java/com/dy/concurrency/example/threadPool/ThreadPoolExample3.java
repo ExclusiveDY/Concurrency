c763f3da39a28f67e8394fe7baa98cf26fe16cb8
strive <398251637@qq.com>
2020-03-31 16:47:11 +0800

    thread pool

---
+++ src/main/java/com/dy/concurrency/example/threadPool/ThreadPoolExample3.java
@@ -0,0 +1,21 @@
+package com.dy.concurrency.example.threadPool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
+public class ThreadPoolExample3 {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newSingleThreadExecutor();
+        for (int i = 0; i < 10; i++) {
+            final int index = i;
+            service.execute(() -> {
+                log.info("task:{}", index);
+            });
+        }
+        service.shutdown();
+    }
+
+}
---
+++ src/main/java/com/dy/concurrency/example/threadPool/ThreadPoolExample4.java
@@ -0,0 +1,29 @@
+package com.dy.concurrency.example.threadPool;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+public class ThreadPoolExample4 {
+    public static void main(String[] args) {
+        ScheduledExecutorService service = Executors.newScheduledThreadPool(3);
+//        service.schedule(new Runnable() {
+//            @Override
+//            public void run() {
+//                log.warn("schedule run");
+//            }
+//        }, 3, TimeUnit.SECONDS);
+        service.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                log.warn("schedule run");
+            }
+        }, 1, 3, TimeUnit.SECONDS);
+//        service.shutdown();
+    }
+
+}
